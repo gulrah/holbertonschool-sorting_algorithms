4b38bb519242386391e20ac01f3de8b74140f1cf
aishalieva0 <aysalieva6@gmal.com>
2024-01-20 12:48:40 -0800

    fx error

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,27 +1,27 @@
 #include "sort.h"
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order using Bubble Sort
+ * bubble_sort - Sorts array of integers in ascending order using Bubble Sort
  * @array: The array to be sorted
  * @size: The size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
+	size_t i, j;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                /* Swap elements if they are in the wrong order */
-                swap(&array[j], &array[j + 1]);
-                /* Print array after each swap */
-                print_array(array, size);
-            }
-        }
-    }
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				/* Swap elements if they are in the wrong order */
+				swap(&array[j], &array[j + 1]);
+				/* Print array after each swap */
+				print_array(array, size);
+			}
+		}
+	}
 }
 
 /**
@@ -31,7 +31,7 @@ void bubble_sort(int *array, size_t size)
  */
 void swap(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
