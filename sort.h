14b9aaeeba5afc6ce4fb500f2f351c55effdb3c6
aishalieva0 <aysalieva6@gmal.com>
2024-01-20 12:54:01 -0800

    solving errors

--- sort.h
+++ sort.h
@@ -1,6 +1,5 @@
 #ifndef SORT_H
 #define SORT_H
-
 #include <stddef.h>
 /**
  * struct listint_s - Doubly linked list node
@@ -11,9 +10,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /* Function prototypes */
