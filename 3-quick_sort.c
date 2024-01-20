e16797302a96824321231d16839dd177d8110845
aishalieva0 <aysalieva6@gmal.com>
2024-01-20 13:08:19 -0800

    betty

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,10 +7,10 @@
  */
 void quick_sort(int *array, size_t size)
 {
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    quick_sort_recursive(array, 0, size - 1, size);
+	quick_sort_recursive(array, 0, size - 1, size);
 }
 
 /**
@@ -22,14 +22,14 @@ void quick_sort(int *array, size_t size)
  */
 void quick_sort_recursive(int *array, int low, int high, size_t size)
 {
-    int pivot;
+	int pivot;
 
-    if (low < high)
-    {
-        pivot = lomuto_partition(array, low, high, size);
-        quick_sort_recursive(array, low, pivot - 1, size);
-        quick_sort_recursive(array, pivot + 1, high, size);
-    }
+	if (low < high)
+	{
+		pivot = lomuto_partition(array, low, high, size);
+		quick_sort_recursive(array, low, pivot - 1, size);
+		quick_sort_recursive(array, pivot + 1, high, size);
+	}
 }
 
 /**
@@ -43,28 +43,28 @@ void quick_sort_recursive(int *array, int low, int high, size_t size)
  */
 int lomuto_partition(int *array, int low, int high, size_t size)
 {
-    int pivot = array[high];
-    int i = low - 1, j, temp;
+	int pivot = array[high];
+	int i = low - 1, j, temp;
 
-    for (j = low; j <= high - 1; j++)
-    {
-        if (array[j] <= pivot)
-        {
-            i++;
-            temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-            if (i != j)
-                print_array(array, size);
-        }
-    }
-    temp = array[i + 1];
-    array[i + 1] = array[high];
-    array[high] = temp;
-    if (i + 1 != high)
-        print_array(array, size);
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+			if (i != j)
+				print_array(array, size);
+		}
+	}
+	temp = array[i + 1];
+	array[i + 1] = array[high];
+	array[high] = temp;
+	if (i + 1 != high)
+		print_array(array, size);
 
-    return (i + 1);
+	return (i + 1);
 }
 
 /**
@@ -74,7 +74,7 @@ int lomuto_partition(int *array, int low, int high, size_t size)
  */
 void swap(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
